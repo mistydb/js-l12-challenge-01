dfbbd1c8acca6c19d4e779e8200f23ff81ff48d5
mistydb <mistydbaird@gmail.com>
2022-09-24 11:29:41 -0600

    added functions to choose a random image to display

--- js/script.js
+++ js/script.js
@@ -2,11 +2,32 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
 const getImage = async function () {
     const res = await fetch(
         "https://picsum.photos/v2/list?limit=100"
     );
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images);
 };
-getImage();
+
+const selectRandomImage = function(images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    // console.log(randomIndex); // check to see if randomIndex is working as expected
+    const randomImage = images[randomIndex];
+    // console.log(randomImage); // check to see if randomImage is working as expected
+    displayImage(randomImage);
+};
+
+const displayImage = function(randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function(
+    getImage();
+));
